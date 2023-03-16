5dfb625d4737aa78d8e3915ad6ec58e68deaaa2a
zinzo1019 <topjoy22@naver.com>
2023-03-16 19:48:32 +0900

    [Fix] Respository와 Service 파일 Boarad -> Post 이름 바꾸기

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -1,10 +1,8 @@
 package com.study.board.service;
 
-import com.study.board.entity.Board;
+import com.study.board.entity.Post;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,19 +13,19 @@ public class BoardService {
     private BoardRepository boardRepository; //인터페이스라 객체 생성 불가, new로 객체를 생성하지만 *9줄
 
     //글 작성 처리
-    public void write(Board board){
+    public void write(Post post){
 
-        boardRepository.save(board);
+        boardRepository.save(post);
     }
 
     //게시글 리스트 처리
-    public List<Board> boardList(){
+    public List<Post> boardList(){
 
         return boardRepository.findAll();
     }
 
     //특정 게시글 불러오기
-    public Board boardView(Integer id){
+    public Post boardView(Integer id){
 
         return boardRepository.findById(id).get(); //그냥 findById로 받으면 Optional로 받아서 오류. get함수 사용.
     }
