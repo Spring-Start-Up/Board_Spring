a1db51594b528acd9416867f21820092d2fb660e
zinzo1019 <topjoy22@naver.com>
2023-03-16 19:47:45 +0900

    [Fix] 어노테이션 붙이기 + 외래키 설정하기

--- src/main/java/com/study/board/entity/Post.java
+++ src/main/java/com/study/board/entity/Post.java
@@ -1,17 +1,31 @@
 package com.study.board.entity;
 
-import javax.persistence.Entity;
+import lombok.*;
 
+import javax.persistence.*;
+
+/** 게시글 */
 @Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post extends BaseEntity{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pno;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user")
     private User user;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "board")
     private Board board;
 
     private String title;
 
     private String content;
+
 }
